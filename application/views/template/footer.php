364f265f9a348c384f1635803696b794afe85f9c
helmutc <helmutc@gmail.com>
2014-02-11 12:49:09 +0000

    Create footer.php

---
+++ application/views/template/footer.php
@@ -0,0 +1,8 @@
+        <div id="footerwrap">
+        <div id="footer">
+            <p>Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
