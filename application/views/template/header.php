fd7552cf272c8b4789cec23e4fdd66ad8befdc5d
helmutc <helmutc@gmail.com>
2014-02-11 12:48:37 +0000

    Create header.php

---
+++ application/views/template/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/template.css" />
+	<script type='text/javascript' src='assets/js/hora.js'></script>
+	<script src='assets/js/jquery-1.7.2.min.js'></script>
+	<script type='text/javascript' src='assets/js/jquery.min.js'></script>
+</head>
+<body>
+    <div id="wrapper">
+        <div id="headerwrap">
+        <div id="header">
+            <img src="assets/images/cmh.jpg" width="90px" height="90px" alt="logo" style="position:fixed;border-radius:80px;margin:5px 0 0 5px;" />
+		<div style="margin:40px auto auto 110px;font-size:24px;color:white;"><b>Centro de Medicina Hiperb&aacute;rica</b>
+		<span style="font-size:10px;float:right;"><right>Vers&atilde;o 3.0</right></span><br />
+		<p style="font-size:11px;float:right;padding-top:15px;"><? $data 'oi' $greet ?> e s&atilde;o <span id="relogio"></span></b></p></div>
+        </div>
+        </div>
+        <div id="navigationwrap">
+        <div id="navigation">
+            <p>This is the Menu</p>
+        </div>
+        </div>
+        <div id="contentliquid"><div id="contentwrap">
